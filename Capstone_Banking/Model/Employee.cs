49616109257c0c8dc1dfbd8aa58dd580ddcd1713
amogh-commits <amogh.kharche@aurionpro.com>
2024-09-30 15:48:48 +0530

    Emoployee Added

---
+++ Capstone_Banking/Model/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Capstone_Banking.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public int ClientId { get; set; }  
+        public DateTime CreatedAt { get; set; }
+       // public ICollection<SalaryDisbursement> SalaryDisbursements { get; set; }  
+    }
+
+}
